767545bcb59dc4ef5023ea64950070b9c2cb1369
facusteckler86 <facundomsteckler@gail.com>
2023-02-27 19:46:16 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-function inicio(){
+// function inicio(){
 
-    var btnCalcular = document.getElementById ("btnCalcular");
-    btnCalcular.addEventListener("Calculando", botonBtnCalcular);
-}
+//     let btnCalcular = document.getElementById ("btnCalcular");
+//     btnCalcular.addEventListener("Calculando", botonBtnCalcular);
+// }
 
-function botonBtnCalcular (){
+// function botonBtnCalcular (){
     
-    var txtPeso = document.getElementById ("txtPeso");
-    var peso = txtPeso.value;
-    alert (peso)
+//     let txtPeso = document.getElementById ("txtPeso");
+//     let peso = txtPeso.value;
+//     alert (peso)
 
-    var txtAltura = document.getElementById ("txtAltura");
-    var altura = txtAltura.value;
-    alert(altura)
+//     let txtAltura = document.getElementById ("txtAltura");
+//     let altura = txtAltura.value;
+//     alert(altura)
     
-    alert("Calculando");
+//     alert("Calculando");
 
-    var imc = peso / (altura * altura);
-    alert ("Su IMC es : " + Math.round(imc));
+//     let imc = peso / (altura * altura);
+//     alert ("Su IMC es : " + Math.round(imc));
 
 
+// }
+
+let aprobado = 7;
+let recuperatorio = 6;
+
+console.log(prompt("Favor de ingresar su nota:  "));
+if(nota => 7){
+    console.log(prompt("aprobado"));
+} else if (nota >7 +ii){
+    console.log(prompt("Usted puede recuperar"));
+}else{
+    console.log(nota >7);
+
+    alert("Usted desapobo, suerte la proxima")
 }
